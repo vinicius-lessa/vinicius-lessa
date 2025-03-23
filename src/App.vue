d18b21cdf497642ab416e8da4c72d51c5745b043
Vinícius Lessa <vinicius.lessa33@outlook.com>
2025-03-23 12:23:03 -0300

    Initi dev my website with Vue components

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<script setup>
+    // import TheWelcome from './components/TheWelcome.vue'
+    import Header from './components/Header.vue'
+    import Main from './components/Main.vue'
+</script>
+
+<template>
+  <header>
+      <Header />
+  </header>
+
+  <main>
+      <Main />
+  </main>
+</template>
+
+<style scoped>
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,23 @@
+<script setup>
+// defineProps({
+//   msg: {
+//     type: String,
+//     required: true,    
+//   },
+// })
+</script>
+
+<template>
+  <div>
+    <ul>
+        <li><a href="#welcome">Welcome</a></li>
+        <li><a href="#about">About</a></li>
+        <li><a href="#skills">Skills</a></li>
+        <li><a href="#projects">Projects</a></li>
+        <li><a href="#contact">Contact</a></li>
+    </ul>
+  </div>
+</template>
+
+<style scoped>
+</style>
