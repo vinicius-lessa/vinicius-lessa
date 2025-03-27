d614799d6b359c9263c50d66359af08b4383ae2a
Vinícius Lessa <vinicius.lessa33@outlook.com>
2025-03-26 22:34:44 -0300

    Testes com componentes vue

--- vue-app/src/App.vue
+++ vue-app/src/App.vue
@@ -1,17 +1,21 @@
 <script setup>
-    // import TheWelcome from './components/TheWelcome.vue'
-    import Header from './components/Header.vue'
-    import Main from './components/Main.vue'
+
+// import TheWelcome from './components/TheWelcome.vue'
+import Header from './components/Header.vue'
+import Main from './components/Main.vue'
+
+const nowInSaoPaulo = new Date().toLocaleString("en-US", { timeZone: "America/Sao_Paulo" });
+const hour = new Date(nowInSaoPaulo).getHours();
+const welcomeMsg = hour < 12 ? "Bom dia" : hour < 18 ? "Boa tarde" : "Boa noite";
+
 </script>
 
-<template>
-  <header>
-      <Header />
-  </header>
+<template>  
+    <Header v-bind:welcomeMsg="welcomeMsg" />
 
-  <main>
-      <Main />
-  </main>
+    <main>
+        <Main />
+    </main>
 </template>
 
 <style scoped>
--- vue-app/src/components/Header.vue
+++ vue-app/src/components/Header.vue
@@ -1,22 +1,23 @@
 <script setup>
-// defineProps({
-//   msg: {
-//     type: String,
-//     required: true,    
-//   },
-// })
+defineProps({
+welcomeMsg: {
+    type: String,
+    required: true,
+},
+})
 </script>
 
 <template>
-  <div>
-    <ul>
-        <li><a href="#welcome">Welcome</a></li>
-        <li><a href="#about">About</a></li>
-        <li><a href="#skills">Skills</a></li>
-        <li><a href="#projects">Projects</a></li>
-        <li><a href="#contact">Contact</a></li>
-    </ul>
-  </div>
+    <header>
+        <ul>
+            <li><a href="#welcome">Welcome</a></li>
+            <li><a href="#about">About</a></li>
+            <li><a href="#skills">Skills</a></li>
+            <li><a href="#projects">Projects</a></li>
+            <li><a href="#contact">Contact</a></li>
+        </ul>
+        <p>{{ welcomeMsg }}</p>
+    </header>
 </template>
 
 <style scoped>
