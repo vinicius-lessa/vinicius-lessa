25ea111ae0170fbb20497da38840d64131aa4d37
Vinícius Lessa <vinicius.lessa33@outlook.com>
2025-07-28 11:43:35 -0300

    add language selector component on the header

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,25 +1,33 @@
-<script setup></script>
+<script setup>
+import { useLanguage } from '../composables/useLanguage.js'
+import LanguageSelector from './LanguageSelector.vue'
+
+const { t } = useLanguage()
+
+</script>
 
 <template>
   <header
     class="fixed top-0 left-0 right-0 bg-white dark:bg-gray-800 shadow-md py-4 z-50"
   >
-    <ul
-      class="flex justify-center space-x-4 text-lg font-semibold text-gray-700 dark:text-gray-200"
-    >
-      <li class="text-gray-700 dark:text-gray-200 text-xl">
-        <a href="#about">About</a>
-      </li>
-      <li class="text-gray-700 dark:text-gray-200 text-xl">
-        <a href="#stack">Stack</a>
-      </li>
-      <li class="text-gray-700 dark:text-gray-200 text-xl">
-        <a href="#projects">Projects</a>
-      </li>
-      <li class="text-gray-700 dark:text-gray-200 text-xl">
-        <a href="#contact">Contact</a>
-      </li>
-    </ul>
+    <div class="flex justify-between items-center max-w-6xl mx-auto px-4">
+      <ul class="flex justify-center space-x-4 text-lg font-semibold text-gray-700 dark:text-gray-200 flex-1">
+        <li class="text-gray-700 dark:text-gray-200 text-xl">
+          <a href="#about">{{ t('header.about') }}</a>
+        </li>
+        <li class="text-gray-700 dark:text-gray-200 text-xl">
+          <a href="#stack">{{ t('header.stack') }}</a>
+        </li>
+        <li class="text-gray-700 dark:text-gray-200 text-xl">
+          <a href="#projects">{{ t('header.projects') }}</a>
+        </li>
+        <li class="text-gray-700 dark:text-gray-200 text-xl">
+          <a href="#contact">{{ t('header.contact') }}</a>
+        </li>
+      </ul>
+      
+      <LanguageSelector />
+    </div>
   </header>
 </template>
 
---
+++ src/components/LanguageSelector.vue
@@ -0,0 +1,39 @@
+<script setup>
+import { useLanguage } from "../composables/useLanguage.js";
+
+const { locale, setLocale } = useLanguage();
+
+const languages = [
+  {
+    code: "pt-BR",    
+    name: "Português",
+    flag: "./assets/icons/brazil.png",
+  },
+  {
+    code: "en",
+    name: "English",
+    flag: "./assets/icons/united-states.png",
+  },
+];
+</script>
+
+<template>
+  <div class="flex gap-1">
+    <button
+      v-for="lang in languages"
+      :key="lang.code"
+      @click="setLocale(lang.code)"
+      :class="[
+        'p-2 rounded-md transition-all duration-200 hover:bg-gray-200 dark:hover:bg-gray-600',
+        locale === lang.code
+          ? 'ring-1 ring-blue-500 dark:ring-blue-300'
+          : '',
+      ]"
+      :title="lang.name"
+    >
+      <img :src="lang.flag" alt="Flag" class="w-6 h-6 rounded-full" />
+    </button>
+  </div>
+  <!-- <a href="https://www.flaticon.com/free-icons/united-states" title="united states icons">United states icons created by Freepik - Flaticon</a> -->
+  <!-- <a href="https://www.flaticon.com/free-icons/brazil" title="brazil icons">Brazil icons created by Freepik - Flaticon</a> -->
+</template>
